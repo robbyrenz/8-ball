1a51eb88bf1805f239440b185ad67472a79dc88e
Robby Renz <nxgrenz@gmail.com>
2019-09-13 19:35:42 -0500

    made output of program pretty

--- program.py
+++ program.py
@@ -5,7 +5,8 @@ import random
 
 def main():
     """The main function of this program"""
-    print(random_answer())
+    answer = random_answer()
+    print_answer(answer)
 
 
 def random_answer():
@@ -14,6 +15,12 @@ def random_answer():
     answers = ['It is certain', 'Without a doubt', 'You may rely on it', 'Yes definitely', 'It is decidedly so', 'As I see it, yes', 'Most likely', 'Yes', 'Outlook good', 'Signs point to yes', 'Reply hazy try again', 'Better not tell you now', 'Ask again later', 'Cannot predict now', 'Concentrate and ask again', 'Don’t count on it', 'Outlook not so good', 'My sources say no', 'Very doubtful', 'My reply is no']
     answer = random.choice(answers)
     return answer
+
+
+def print_answer(answer):
+    print()
+    print(f"\t\t\t8-Ball says: \"{answer}\"")
+    print()
 
 
 # call main

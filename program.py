b7738305c70c67047e6e09cf651646d507054bcf
Robby Renz <nxgrenz@gmail.com>
2019-09-13 19:31:01 -0500

    main functionality of program complete

--- program.py
+++ program.py
@@ -1,4 +1,4 @@
-# A program I made in 13/9/2019 on a Friday that simulates the feel of an 8-Ball
+# A program made in 13/9/2019 on a Friday that simulates the feel of an 8-Ball
 
 import random
 
@@ -10,7 +10,8 @@ def main():
 
 def random_answer():
     """Randomly returns an answer from a list"""
-    answers = []
+    # there are 20 answers in the below list (called answers)
+    answers = ['It is certain', 'Without a doubt', 'You may rely on it', 'Yes definitely', 'It is decidedly so', 'As I see it, yes', 'Most likely', 'Yes', 'Outlook good', 'Signs point to yes', 'Reply hazy try again', 'Better not tell you now', 'Ask again later', 'Cannot predict now', 'Concentrate and ask again', 'Don’t count on it', 'Outlook not so good', 'My sources say no', 'Very doubtful', 'My reply is no']
     answer = random.choice(answers)
     return answer
 
